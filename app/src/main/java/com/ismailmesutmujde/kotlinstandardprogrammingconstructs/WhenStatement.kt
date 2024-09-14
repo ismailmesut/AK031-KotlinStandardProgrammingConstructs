8078f1a313a12eb838db04b561b38f01a4d18153
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-14 21:59:10 +0300

    updated: when statement

---
+++ app/src/main/java/com/ismailmesutmujde/kotlinstandardprogrammingconstructs/WhenStatement.kt
@@ -0,0 +1,17 @@
+package com.ismailmesutmujde.kotlinstandardprogrammingconstructs
+
+fun main() {
+
+    val day = 3
+
+    when(day) {
+        1 -> println("Monday")
+        2 -> println("Tuesday")
+        3 -> println("Wednesday")
+        4 -> println("Thursday")
+        5 -> println("Friday")
+        6 -> println("Saturday")
+        7 -> println("Sunday")
+        else -> println("There is no such day")
+    }
+}
